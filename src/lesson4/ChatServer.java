d31a4be1ff183162d81f24746e99b3bd6d9b5f73
Vasiliy Krasichkov <krasichkov.v@gmail.com>
2020-04-16 15:31:31 +0300

    выполнение дз

--- src/lesson4/ChatServer.java
+++ src/lesson4/ChatServer.java
@@ -1,5 +1,10 @@
 package lesson4;
 
 public class ChatServer {
-
+    public void start(int port){
+        System.out.println("Server start at port: " + port);
+    }
+    public void stop(){
+        System.out.println("Server stopped!");
+    }
 }
